3995931dc643bf4fac032d7b425f5d74c8c71be2
lMario <luismariovasquezcorrea@gmail.com>
2024-09-11 21:24:44 -0500

    Event class

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from datetime import datetime, date, time
+from datetime import datetime, date, time, timedelta
 from typing import ClassVar
 
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
@@ -21,3 +21,37 @@ class Reminder:
 
     def __str__(self) -> str:
         return f"Reminder on {self.date_time} of type {self.type}"
+
+
+@dataclass
+
+class Event:
+    
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder] = field(default_factory = list)
+    id: str = field(default_factory = generate_unique_id)
+
+    def add_reminder(self, date_time: datetime, reminder_type: str = Reminder.EMAIL):
+        reminder = Reminder(date_time = date_time, type = reminder_type)
+        self.reminders.append(reminder)
+    
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+    
+    def __str__(self) -> str:
+        return (f"ID: {self.id}\n"
+                f"Event title: {self.title}\n"
+                f"Description: {self.description}\n"
+                f"Time: {self.start_at} - {self.end_at}")
+    
+
+
+
+
